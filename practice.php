952d7dbff9875409a0cf5762a0fd4a3a36f83656
粟野 <yuuuuuna39@gmail.com>
2021-01-30 05:35:17 +0000

    first commit

---
+++ practice.php
@@ -0,0 +1,45 @@
+<?php
+echo 'hello php';
+
+// 課題１
+$a =3;
+$b =7;
+echo $a +$b;
+
+// 課題２
+$array_month= ["january","february","march","april","may","june","july","august","september","october","november","december"];
+echo $array_month[7];
+
+// 課題３
+$hello ="Hello,";
+$name ="yuna";
+$world = "' World!";
+
+echo $hello;
+echo $name;
+echo $world;
+
+// 課題４
+$tech_boost = 'tech';
+$tech_boost .= ' _boost';
+
+echo $tech_boost;
+
+// 課題５
+$calendar = [
+    "January " => "1月",
+    "February" => "2月",
+    "March" => "3月",
+    "April" => "4月",
+    "May" =>"5月",
+    "June" => "6月",
+    "July" => "7月",
+    "August" => "8月",
+    "September" => "9月",
+    "October" => "10月",
+    "November" => "11月",
+    "December" => "12月",
+    ];
+    
+    echo $calendar["December"];
+
