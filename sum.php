27a72635b47e9383d620561d035b2d2bc5179b7e
粟野 <yuuuuuna39@gmail.com>
2021-02-02 13:13:59 +0000

    first commit

---
+++ sum.php
@@ -0,0 +1,22 @@
+<?php
+
+// 課題１
+function sum($i){
+      $result = $i*2;
+      return $result;
+}
+
+    echo sum(4);
+
+    
+
+// <!--課題２-->
+function f($a,$b){
+     $result = $a+$b;
+     return $result;
+}
+
+    echo f(4,2);
+
+
+?>
