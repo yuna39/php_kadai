c39560e46727beace5fa8fb28305d4675b7270f4
粟野 <yuuuuuna39@gmail.com>
2021-02-06 09:41:37 +0000

    課題04

--- sum.php
+++ sum.php
@@ -1,6 +1,6 @@
 <?php
 
-// // 課題１
+// // // 課題１
 function sum($i){
       $result = $i*2;
       return $result;
@@ -10,7 +10,7 @@ function sum($i){
 
     
 
-// // 課題２
+// // // // 課題２
 function f($a,$b){
      $result = $a+$b;
      return $result;
@@ -19,8 +19,8 @@ function f($a,$b){
     echo f(4,2);
 
 
-// 課題３
 
+// // // 課題３
 function total($arr){
     $result = 1;
     
@@ -35,7 +35,8 @@ function total($arr){
     echo "\n";
     
     
-// 課題４
+    
+// // // 課題４
 function max_array($arr){
     $max_number = $arr[0];
     foreach($arr as $a){
@@ -47,4 +48,41 @@ function max_array($arr){
 }
     max_array(array(3,6,4,7));
     echo "\n";
+    
+    
+    
+// // 課題５
+
+// // strip_tags・・・htmlタグを取り除く（<>の部分を除去）
+$string = '<a href="http://google.com">Google</a>';
+$result = strip_tags($string);
+echo $result;
+
+
+// // array_push・・・一つ以上の要素を配列の最後に追加する
+$array = ["orange","peach"];
+echo $array[0];
+$arraylength = array_push($array,"apple");
+echo $arraylength;
+
+
+// array_merge・・・一つまたは複数の配列を結合
+$array1 = [1,2];
+$array2 = [11,22];
+$array = array_merge($array1, $array2);
+echo $array[2];
+
+
+// // time,mktime・・・タイムスタンプを取得するためのメソッド
+$time = mktime();
+var_dump(date('y年m月d日h時i分s秒',$time));
+print('<br/>');
+
+
+// // date・・・日付の設定
+date_default_timezone_set('Asia/Tokyo ');
+echo '<p>', date('y年m月d日h時i分s秒'),'</p>';
+
+
 ?>
+
