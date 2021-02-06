cd98fd9f90faafa9930031e238c7ef708c753aca
粟野 <yuuuuuna39@gmail.com>
2021-02-06 07:17:01 +0000

    課題04

--- sum.php
+++ sum.php
@@ -1,6 +1,6 @@
 <?php
 
-// 課題１
+// // 課題１
 function sum($i){
       $result = $i*2;
       return $result;
@@ -10,7 +10,7 @@ function sum($i){
 
     
 
-// <!--課題２-->
+// // 課題２
 function f($a,$b){
      $result = $a+$b;
      return $result;
@@ -19,4 +19,32 @@ function f($a,$b){
     echo f(4,2);
 
 
+// 課題３
+
+function total($arr){
+    $result = 1;
+    
+    foreach($arr as $ar){
+    
+    $result *= $ar;
+    }
+    echo $result;
+}
+    
+    total(array(1,3,5,7,9));
+    echo "\n";
+    
+    
+// 課題４
+function max_array($arr){
+    $max_number = $arr[0];
+    foreach($arr as $a){
+        if($max_number < $a){
+            $max_number = $a;
+        }
+    }
+    echo $max_number;
+}
+    max_array(array(3,6,4,7));
+    echo "\n";
 ?>
